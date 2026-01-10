bc2cfe862b7ad7eca780e30ebb35a0bee64d639f
AbinashG <abhi24964@gmail.com>
2026-01-10 23:09:30 +0530

    added dynamic webapp code

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import asyncio
 
 app = FastAPI(title="Dynamic Profile App")
 
@@ -17,6 +18,17 @@ async def home(request: Request):
         {"request": request}
     )
 
+@app.get("/about", response_class=HTMLResponse)
+async def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
+
+@app.get("/projects", response_class=HTMLResponse)
+async def projects(request: Request):
+    return templates.TemplateResponse("projects.html", {"request": request})
+
+@app.get("/skills", response_class=HTMLResponse)
+async def skills(request: Request):
+    return templates.TemplateResponse("skills.html", {"request": request})
 
 @app.post("/feedback")
 async def submit_feedback(
@@ -26,7 +38,26 @@ async def submit_feedback(
 ):
     # Later this will go to DB / S3 / DynamoDB
     print(name, email, suggestion)
+    #await asyncio.sleep(5)  # Simulate async processing
 
     return {
         "message": "Feedback submitted successfully"
     }
+
+
+@app.get("/feedback-form", response_class=HTMLResponse)
+async def feedback_form(request: Request):
+    return templates.TemplateResponse("feedback_form.html", {"request": request})
+
+
+@app.get("/blocking")
+def blocking_operation():
+    import time
+    time.sleep(5)  # Simulate a blocking operation
+    return {"message": "Blocking operation completed"}
+
+
+@app.get("/non-blocking")
+async def non_blocking_operation():
+    await asyncio.sleep(5)  # Simulate a non-blocking operation
+    return {"message": "Non-blocking operation completed"}
